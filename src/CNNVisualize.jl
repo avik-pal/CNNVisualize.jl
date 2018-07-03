89e498e3b7384a7d479f457357baa137eb8a6ed2
Avik Pal <avikpal@iitk.ac.in>
2018-07-03 17:14:13 +0530

    Add dependencies and file structure

--- src/CNNVisualize.jl
+++ src/CNNVisualize.jl
@@ -1,5 +1,12 @@
 module CNNVisualize
 
-# package code goes here
+using Flux, Plots, Metalhead, Images, Augmentor
+using Flux: Tracker
+using Flux.Tracker: data
+
+include("backpropagation.jl")
+include("gradcam.jl")
+include("deepdream.jl")
+include("utils.jl")
 
 end # module
