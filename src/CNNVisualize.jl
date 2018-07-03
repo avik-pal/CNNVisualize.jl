3bc1b1688f9301bba430e2264f413d6edc5ed744
Avik Pal <avikpal@iitk.ac.in>
2018-07-03 21:33:31 +0530

    Add exports

--- src/CNNVisualize.jl
+++ src/CNNVisualize.jl
@@ -1,3 +1,5 @@
+__precompile__()
+
 module CNNVisualize
 
 using Flux, Plots, Metalhead, Images, Augmentor
@@ -8,5 +10,14 @@ include("backpropagation.jl")
 include("gradcam.jl")
 include("deepdream.jl")
 include("utils.jl")
+
+export GradCAM, GuidedGradCAM, Backprop, VanillaBackprop, GuidedBackprop,
+       DeconvNet, save_gradient_images, save_gradcam, save_grayscale_gradient,
+       positive_negative_saliency, im2arr_rgb, image_to_arr, load_image, save_image,
+       load_model, deepdream, make_step
+
+# Some re exports
+
+export save, load, cpu, gpu
 
 end # module
